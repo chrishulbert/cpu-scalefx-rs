718b436a8ee2f9726a266b775cb6a3778cb15bb9
Chris <chris.hulbert@gmail.com>
2026-02-08 21:18:27 +1100

    calculate_corner_strengths

--- Readme.md
+++ Readme.md
@@ -4,6 +4,6 @@ ScaleFX pixel art upcaler, for the CPU, in Rust.
 
 ## References
 
-* https://github.com/libretro/slang-shaders/blob/master/edge-smoothing/scalefx/shaders/old/scalefx-9x.slangp
-* https://github.com/libretro/slang-shaders/blob/master/edge-smoothing/scalefx/shaders/old/scalefx-pass0.slang
+* https://github.com/libretro/slang-shaders/tree/master/edge-smoothing/scalefx/shaders
 * http://www.compuphase.com/cmetric.htm
+* https://docs.libretro.com/development/shader/slang-shaders/#pragma-parameter
--- src/main.rs
+++ src/main.rs
@@ -65,14 +65,24 @@ fn colour_distance(a: u32, b: u32) -> f32 {
     (((((512 + r_mean)*r*r)>>8) + 4*g*g + (((767-r_mean)*b*b)>>8)) as f32).sqrt()
 }
 
-
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct PixelWithDistances {
     pixel: u32,
     up_left: f32, // Colour distance to the pixel to the up-left.
     up: f32,
     up_right: f32,
     right: f32,
+}
+impl PixelWithDistances {
+    fn offscreen() -> Self { // The representation for a transparent offscreen pixel.
+        Self {
+            pixel: 0,
+            up_left: MAX_COLOUR_DISTANCE,
+            up: MAX_COLOUR_DISTANCE,
+            up_right: MAX_COLOUR_DISTANCE,
+            right: MAX_COLOUR_DISTANCE,
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -116,6 +126,82 @@ fn calculate_distances(image: &Image) -> ImageWithDistances {
     }
 }
 
+
+#[derive(Debug)]
+struct PixelWithCornerStrengths {
+    pixel: u32,
+    up_left: f32, // Corner strength. Called X in the shader.
+    up_right: f32, // Y in the shader.
+    down_right: f32, // Z in the shader.
+    down_left: f32, // W in the shader.
+}
+
+#[derive(Debug)]
+struct ImageWithCornerStrengths {
+    width: usize,
+    height: usize,
+    pixels: Vec<PixelWithCornerStrengths>,
+}
+
+const THRESHOLD: f32 = 0.5; // Min 0.01; max: 1; step: 0.01
+const IS_FILTER_AA_ENABLED: bool = true;
+
+// https://github.com/libretro/slang-shaders/blob/master/edge-smoothing/scalefx/shaders/scalefx-pass1.slang
+fn corner_strength(d: f32, a_x: f32, a_y: f32, b_x: f32, b_y: f32) -> f32 {
+	let diff = a_x - a_y;
+	let weight_1 = (THRESHOLD - d).max(0.) / THRESHOLD;
+    let is_x_g_y = a_x.min(b_x) + a_x  >  a_y.min(b_y) + a_y;
+    let x_g_y_diff = if is_x_g_y { diff } else { -diff };
+    let weight_2_raw = (1. - d) + x_g_y_diff;
+	let weight_2 = weight_2_raw.clamp(0., 1.);
+	if IS_FILTER_AA_ENABLED || 2. * d < a_x + a_y { weight_1 * weight_2 * a_x * a_y } else { 0. }
+}
+
+// Calculate all the corner strengths.
+// Aka "calculate strength of interpolation candidates" according to the shader comment.
+// This implements pass 1 here:
+// https://github.com/libretro/slang-shaders/blob/master/edge-smoothing/scalefx/shaders/scalefx-pass1.slang
+fn calculate_corner_strengths(image: &ImageWithDistances) -> ImageWithCornerStrengths {
+    let pixels_len = image.pixels.len();
+    let mut pixels: Vec<PixelWithCornerStrengths> = Vec::with_capacity(pixels_len);
+    let offscreen = PixelWithDistances::offscreen();
+
+    for y in 0..image.height {
+        for x in 0..image.width {
+            let i = y * image.width + x;
+
+            // Get the neighbouring pixels, returning transparent if they're out of bounds.
+            let up_left = if y==0 || x==0 { offscreen } else { image.pixels[i - image.width - 1] };
+            let up = if y==0 { offscreen } else { image.pixels[i - image.width] };
+            let left = if x==0 { offscreen } else { image.pixels[i - 1] };
+            let center = image.pixels[i];
+            let right = if x==image.width-1 { offscreen } else { image.pixels[i + 1] };
+            let down_left = if x==0 || y==image.height-1 { offscreen } else { image.pixels[i + image.height - 1] };
+            let down = if y==image.height-1 { offscreen } else { image.pixels[i + image.height] };
+            let down_right = if x==image.width-1 || y==image.height-1 { offscreen } else { image.pixels[i + image.height + 1] };
+
+            // Calculate the corner strengths:
+            let up_left = corner_strength(left.up_right, left.right, center.up, up_left.right, left.up);
+            let up_right = corner_strength(right.up_left, center.right, center.up, up.right, right.up);
+            let down_right = corner_strength(down.up_right, center.right, down.up, down.right, down_right.up);
+            let down_left = corner_strength(down.up_left, left.right, down.up, down_left.right, down_left.up);
+
+            pixels.push(PixelWithCornerStrengths {
+                pixel: center.pixel,
+                up_left,
+                up_right,
+                down_right,
+                down_left,
+            });
+        }
+    }
+    ImageWithCornerStrengths {
+        width: image.width,
+        height: image.height,
+        pixels,
+    }
+}
+
 fn main() {
     println!("Hello, world!");
     println!("{}", colour_distance(0xffffffff, 0xff));
@@ -131,6 +217,7 @@ fn main() {
     println!("{}", colour_distance(0x0000ffff, 0));
 
     let sample = sample();
-    let sample_with_distances = calculate_distances(&sample);
-    println!("{:#?}", sample_with_distances);
+    let distances = calculate_distances(&sample);
+    let corners = calculate_corner_strengths(&distances);
+    println!("{:#?}", corners);
 }
